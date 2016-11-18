4370098bd59250b4e9ae602ec8e9e4ed6140aa23
anartzmugika <mugan86@gmail.com>
2016-11-18 21:01:15 +0100

    Update

--- app/src/main/java/com/anartzmugika/floatingactionmenu/MainActivity.java
+++ app/src/main/java/com/anartzmugika/floatingactionmenu/MainActivity.java
@@ -1,12 +1,14 @@
 package com.anartzmugika.floatingactionmenu;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.MenuItem;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+import com.jaredrummler.android.device.DeviceName;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get Android Device SDK
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+
+        System.out.println("Device SDK is: " + SDK_INT);
+
+        //Important: Add to not show "android.os.NetworkOnMainThreadException at android.os.StrictMode$AndroidBlockGuardPolicy.onNetwork"
+        // This exception is thrown when application attempts to perform a networking operation in the main thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        // Get Device Manufacturer and model without using library
+        System.out.println(Device.getDeviceName());
+
+        //Without using library
+        System.out.println("Manufacturer: " + Device.getDeviceManuFacturer());
+        System.out.println("Product: " + Device.getDeviceProduct());
+        System.out.println("Model: " + Device.getDeviceModel());
+
+
+        //Get Device most important info using 'com.jaredrummler:android-device-names:1.1.2' library
+        DeviceName.DeviceInfo info_device = DeviceName.getDeviceInfo(MainActivity.this);
+        System.out.println("Manufacturer: " + info_device.manufacturer);
+        System.out.println("Model: " + info_device.model);
+        System.out.println("Market Name: " + info_device.marketName);
+        System.out.println("Code Name: " + info_device.codename);
+
+        //Obtain Device Name model and current SDK
+        System.out.println(DeviceName.getDeviceName() + " / SDK: " + SDK_INT);
+
         materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
         floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
         floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
@@ -26,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //TODO something when floating action menu first item clicked
+
 
             }
         });
